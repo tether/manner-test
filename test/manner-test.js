628f3625fabfaa6abc8e54ddc9fedc71b268fb38
bredele <olivier.wietrich@gmail.com>
2017-12-05 17:13:10 -0700

    Proxy service methods

--- test/manner-test.js
+++ test/manner-test.js
@@ -4,10 +4,23 @@
  */
 
 const test = require('tape')
+const manner = require('..')
 
-test('this is an example', assert => {
-  assert.plan(1)
-  assert.equal(1 + 2, 3)
+
+/**
+ * Create test service.
+ * @type {Function}
+ */
+
+const service = manner({
+  get() {
+    return 'hello'
+  }
 })
 
-  
+
+test('should return an object containing service method(s)', assert => {
+  assert.plan(1)
+  assert.equal(typeof service.get, 'function')
+  assert.equal(service.options == null, true)
+})
