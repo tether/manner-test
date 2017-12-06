eea471fd92f2204e938748432daee113eddec28d
bredele <olivier.wietrich@gmail.com>
2017-12-05 17:10:22 -0700

    Proxy service calls and return promise

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+/**
+ * Dependencies.
+ */
+
+const bluff = require('bluff')
+
 
 /**
- * This is a simple description.
+ * Mock up HTTP response as returned by
+ * a manner service.
  *
+ * @param {Object} service (manner's service)
  * @api public
  */
 
-module.exports = function () {
-  // do something
+module.exports = function (service) {
+  return new Proxy({}, {
+    get(target, key, receiver) {
+      const method = service[key]
+      return (path, query, data) => {
+        return bluff(method(path, query, data))
+      }
+    }
+  })
 }
-
-  
